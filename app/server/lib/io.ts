c7d46b2da6349ee6ebb53678448b5258d470a805
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-28 04:58:31 +0100

    add dev mode

--- app/server/lib/io.ts
+++ app/server/lib/io.ts
@@ -1,27 +1,67 @@
-function getArg(name: string): string|void {
-	for (let i = 0; i < process.argv.length; i++) {
-		if (process.argv[i] === `--${name}`) {
-			return process.argv[i + 1];
-		} else if (process.argv[i].startsWith(`--${name}=`)) {
-			return process.argv[i].slice(3 + name.length);
-		}
-	}
-	return void 0;
+/**
+ * Check if an argument with given name is present
+ * 
+ * @param {string} name - The name of the argument
+ * @param {string} [short] - A short name for the argument
+ * 
+ * @returns {boolean} Whether the argument was passed
+ */
+function hasArg(name: string, short?: string): boolean {
+    for (let i = 0; i < process.argv.length; i++) {
+        const arg = process.argv[i];
+        if (arg === `--${name}` || (short && arg === `-${short}`)) {
+            return true;
+        }
+    }
+    return false;
 }
 
-function getNumArg(name: string): number|void {
-	const arg = getArg(name);
-	if (arg === void 0) return void 0;
-	return ~~arg;
+/**
+ * Get a string-type argument by name
+ * 
+ * @param {string} name - The name of the argument
+ * @param {string} [short] - A short name for the argument
+ * 
+ * @returns {string|void} The argument's value or undefined
+ */
+function getArg(name: string, short?: string): string | void {
+    for (let i = 0; i < process.argv.length; i++) {
+        const arg = process.argv[i];
+        if (arg === `--${name}` || (short && arg === `-${short}`)) {
+            return process.argv[i + 1];
+        } else if (arg.startsWith(`--${name}=`)) {
+            return arg.slice(3 + name.length);
+        }
+    }
+    return void 0;
+}
+
+/**
+ * Get a number-type argument by name
+ * 
+ * @param {string} name - The name of the argument
+ * @param {string} [short] - A short name for the argument
+ * 
+ * @returns {number|void} A number-representation of the argument
+ * 	or undefined
+ */
+function getNumArg(name: string, short?: string): number | void {
+    const arg = getArg(name, short);
+    if (arg === void 0) return void 0;
+    return ~~arg;
 }
 
 export type IO = ReturnType<typeof getIO>;
 
+/**
+ * Get all IO
+ */
 export function getIO() {
-	return {
-		ports: {
-			http: getNumArg('http') || undefined,
-			https: getNumArg('https') || undefined
-		}
-	}
+    return {
+        dev: hasArg('dev', 'd'),
+        ports: {
+            http: getNumArg('http') || undefined,
+            https: getNumArg('https') || undefined,
+        },
+    };
 }
