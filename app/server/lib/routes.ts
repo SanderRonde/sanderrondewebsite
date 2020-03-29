ffc32a2843fceca0ce0bfd93ac1b7f6cefe7043a
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-29 06:05:50 +0200

    rewrite contents of files served during development

--- app/server/lib/routes.ts
+++ app/server/lib/routes.ts
@@ -15,28 +15,35 @@ const THESIS_FILE = path.join(
 
 /**
  * Similar to serve-static but allows for specifying a prefix
- * 
+ *
  * @param {string} root - The root of the directory to serve from
- * @param {{ prefix?: string; extensions?: string[] }} options - The 
+ * @param {{ prefix?: string; extensions?: string[] }} options - The
  * 	options for this route
  * @param {string} [options.prefix] - A prefix to "subtract" from
- * 	the url. For example if you want to serve only /node_modules/xyz 
+ * 	the url. For example if you want to serve only /node_modules/xyz
  * 	then you can't serve-static /node_modules/xyz because
  * 	any request to /node_modules/xyz will seach in
  * 	/node_modules/xyz/node_modules/xyz.
  * 	So this version rewrites the URL to remove the prefix
  * @param {string[]} [options.extensions] - Default extensions to
  * 	fall back on if the path was not a match
- * 
+ * @param {rewrite?: (content: string,filename: string) => Promise<string> | string;} [options.rewrite] - Rewrite
+ *  the content of the file dynamically
+ *
  * @returns {express.RequestHandler} - A request handler, for use
  * 	in an express(...).use() call
  */
 function serve(
     root: string,
     {
+        rewrite,
         prefix = '',
         extensions = [],
     }: {
+        rewrite?: (
+            content: string,
+            filename: string
+        ) => Promise<string> | string;
         prefix?: string;
         extensions?: string[];
     } = {}
@@ -59,13 +66,35 @@ function serve(
         ];
         for (const filePath of filePaths) {
             if (await fs.pathExists(filePath)) {
-                res.status(200);
-                res.sendFile(filePath);
-                return;
+                const stat = await fs.stat(filePath);
+                if (stat.isFile()) {
+                    res.status(200);
+                    if (rewrite) {
+                        const content = await fs.readFile(filePath, {
+                            encoding: 'utf8',
+                        });
+                        res.contentType(path.basename(filePath));
+                        res.send(await rewrite(content, filePath));
+                        res.end();
+                    } else {
+                        res.sendFile(filePath);
+                    }
+                    return;
+                }
             }
         }
         next();
     };
+}
+
+/**
+ * Rewrite all node-style imports to be relative
+ * So 'lit-html' -> '/node_modules/lit-html'
+ */
+function rewriteModuleImports(content: string) {
+    return content
+        .replace(/['"]wc-lib['"]/g, '\'/node_modules/wc-lib/build/es/wc-lib.js\'')
+        .replace(/['"]lit-html['"]/g, '\'/node_modules/lit-html/lit-html.js\'');
 }
 
 /**
@@ -82,10 +111,24 @@ export function initRoutes({ app, io }: WebServer) {
 	);
     if (io.dev) {
         app.use(
-            serveStatic(path.join(CLIENT_DIR, 'src'), {
-                index: ['entrypoints/index/index.html'],
+            serve(path.join(CLIENT_DIR, 'src'), {
+                rewrite: rewriteModuleImports,
             })
         );
+        app.get('/', async (_req, res) => {
+            const filePath = path.join(
+                CLIENT_DIR,
+                'src',
+                'entrypoints/index/index.html'
+            );
+            const content = await fs.readFile(filePath, {
+                encoding: 'utf8',
+            });
+            res.contentType(path.basename(filePath));
+            res.send(rewriteModuleImports(content));
+            res.end();
+        });
+
         const litHTMLSubpath = 'node_modules/lit-html';
         app.use(serve(path.join(ROOT_DIR, litHTMLSubpath), {
             extensions: ['js'],
