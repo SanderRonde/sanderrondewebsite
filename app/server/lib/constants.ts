c7470aaf16ad8786b9f6f25499c3a23f328ce9d1
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-03-30 02:10:10 +0200

    add description

--- app/server/lib/constants.ts
+++ app/server/lib/constants.ts
@@ -5,6 +5,7 @@ const DIR_SCHEME = (import.meta as any).url as string;
 const DIR_FULL = path.dirname(DIR_SCHEME.split('file://')[1]);
 const DIR_NAME = os.platform() === 'win32' ? DIR_FULL.slice(1) : DIR_FULL;
 
+// Paths
 export const ROOT_DIR = path.join(DIR_NAME, '../../../');
 export const APP_DIR = path.join(ROOT_DIR, 'app');
 export const CLIENT_DIR = path.join(APP_DIR, 'client');
