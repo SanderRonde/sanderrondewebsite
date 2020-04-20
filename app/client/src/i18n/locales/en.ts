56a368e5af9b8ec8d0edfe858324ec6c8d204125
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-20 02:07:04 +0200

    add new i18n entries

--- app/client/src/i18n/locales/en.ts
+++ app/client/src/i18n/locales/en.ts
@@ -1,6 +1,29 @@
 import { I18NRoot } from '../spec';
 
 export const messages = {
+	generic: {
+		dismiss: {
+			message: 'dismiss'
+		},
+		reload: {
+			message: 'reload'
+		}
+	},
+	index: {
+		hi: {
+			message: 'Hi!',
+		}
+	},
+	shared: {
+		sw: {
+			works_offline: {
+				message: 'Works offline now',
+			},
+			update_ready: {
+				message: 'Page can be updated'
+			}
+		}
+	}
 };
 
 export default messages as I18NRoot;
--- app/client/src/i18n/locales/nl.ts
+++ app/client/src/i18n/locales/nl.ts
@@ -1,6 +1,29 @@
 import { I18NRoot } from '../spec';
 
 export const messages = {
+	generic: {
+		dismiss: {
+			message: 'verberg'
+		},
+		reload: {
+			message: 'opnieuw laden'
+		}
+	},
+	index: {
+		hi: {
+			message: 'Hoi!',
+		}
+	},
+	shared: {
+		sw: {
+			works_offline: {
+				message: 'Pagina werkt offline',
+			},
+			update_ready: {
+				message: 'Pagina kan worden geüpdate'
+			}
+		}
+	}
 };
 
 export default messages as I18NRoot;
