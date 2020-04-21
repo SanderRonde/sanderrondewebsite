330dbd2c0c3fdb03480e95c285d31aef43d7424d
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-21 04:41:16 +0200

    add spdy server

--- app/server/app.ts
+++ app/server/app.ts
@@ -5,7 +5,11 @@ import { Routes } from './lib/routes.js';
 import { IO } from './lib/io.js';
 import express from 'express';
 import morgan from 'morgan';
+import fs from 'fs-extra';
 import http from 'http';
+import spdy from 'spdy';
+import path from 'path';
+import { ROOT_DIR } from './lib/constants.js';
 
 /**
  * The main app class used to wrap everything up
@@ -13,10 +17,13 @@ import http from 'http';
 export class WebServer {
 	public app!: express.Express;
 
-	constructor(public io: IO.IO) {
+	constructor(public io: IO.IO) {}
+
+	async init() {
 		this._initVars();
 		this._initRoutes();
 		this._listen();
+		return this;
 	}
 
 	/**
@@ -48,12 +55,22 @@ export class WebServer {
 	/**
 	 * Start listening for requests
 	 */
-	private _listen() {
-		// HTTPS is unused for now since this is behind a proxy
+	private async _listen() {
+		const config: spdy.server.ServerOptions = {
+			cert: await fs.readFile(path.join(ROOT_DIR, 'certs', 'cert.crt')),
+			key: await fs.readFile(path.join(ROOT_DIR, 'certs', 'key.key')),
+		};
 		http.createServer(this.app).listen(this.io.ports.http, () => {
 			console.log(`HTTP server listening on port ${this.io.ports.http}`);
+		});
+		spdy.createServer(config, this.app).listen(this.io.ports.https, () => {
+			console.log(
+				`HTTPS server listening on port ${this.io.ports.https}`
+			);
 		});
 	}
 }
 
-new WebServer(IO.getIO());
+(async () => {
+	await new WebServer(IO.getIO()).init();
+})();
