f68419985b22bd139e04a4918989b6ecf749fcbc
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-22 03:16:23 +0200

    rework caching a bit

--- app/server/lib/cache.ts
+++ app/server/lib/cache.ts
@@ -1,30 +1,93 @@
 export namespace Caching {
-    export class CacheStore<CS, V> {
-        private _cacheStates: Map<CS, V> = new Map();
+	export function createStore<CS extends string | number | boolean, V>(
+		complex: false
+	): CacheStore<CS, V>;
+	export function createStore<CS extends object | any[] | Function, V>(
+		complex: true,
+		compare: (stored: CS, current: CS) => boolean
+	): CacheStore<CS, V>;
+	export function createStore<CS, V>(
+		complex: boolean,
+		compare?: (stored: CS, current: CS) => boolean
+	): CacheStore<CS, V> {
+		if (!complex) {
+			return (new SimpleCacheStore<any, V>() as unknown) as CacheStore<
+				CS,
+				V
+			>;
+		} else {
+			return (new ComplexCacheStore<any, V>(
+				compare!
+			) as unknown) as CacheStore<CS, V>;
+		}
+	}
 
-        constructor(private _compare: (stored: CS, current: CS) => boolean) {}
+	export interface CacheStore<CS, V> {
+		has(state: CS): boolean;
+		get(state: CS): V | null;
+		set(state: CS, value: V): void;
+		delete(state: CS): boolean;
+	}
 
-        getCache(cacheState: CS): V | null {
-            for (const [key, value] of this._cacheStates.entries()) {
-                if (this._compare(key, cacheState)) {
-                    return value;
-                }
-            }
-            return null;
-        }
+	class SimpleCacheStore<CS extends string | number | boolean, V>
+		implements CacheStore<CS, V> {
+		private _cacheStates: Map<CS, V> = new Map();
 
-        setCache(cacheState: CS, value: V) {
-            this._cacheStates.set(cacheState, value);
-        }
-    }
+		has(state: CS) {
+			return this._cacheStates.has(state);
+		}
 
-    export class FileCache extends CacheStore<string, string> {
-        constructor() {
-            super((stored, current) => stored === current);
-        }
+		get(state: CS): V | null {
+			const value = this._cacheStates.get(state);
+			if (value === undefined) return null;
+			return value;
+		}
 
-        has(fileName: string) {
-            return this.getCache(fileName) !== null;
-        }
-    }
+		set(state: CS, value: V) {
+			return this._cacheStates.set(state, value);
+		}
+
+		delete(state: CS) {
+			return this._cacheStates.delete(state);
+		}
+	}
+
+	class ComplexCacheStore<CS, V> {
+		private _cacheStates: Map<CS, V> = new Map();
+		private _lastMatch: [CS, V] | null = null;
+
+		constructor(private _compare: (stored: CS, current: CS) => boolean) {}
+
+		private _match(cacheState: CS): V | null {
+			if (
+				this._lastMatch &&
+				this._compare(this._lastMatch[0], cacheState)
+			) {
+				return this._lastMatch[1];
+			}
+
+			for (const [key, value] of this._cacheStates.entries()) {
+				if (this._compare(key, cacheState)) {
+					this._lastMatch = [key, value];
+					return value;
+				}
+			}
+			return null;
+		}
+
+		getCache(cacheState: CS): V | null {
+			return this._match(cacheState);
+		}
+
+		hasCache(cacheState: CS): boolean {
+			return this._match(cacheState) !== null;
+		}
+
+		setCache(cacheState: CS, value: V) {
+			this._lastMatch = [cacheState, value];
+			this._cacheStates.set(cacheState, value);
+		}
+	}
+
+	export class FileCache extends SimpleCacheStore<string, string> {}
 }
