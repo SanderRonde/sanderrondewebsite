cceabadcfd08c0a0bc674e3371c8742c89d33d9c
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-23 00:38:04 +0200

    add scroller

---
+++ app/client/src/components/index/sander-ronde/sander-ronde.css.ts
@@ -0,0 +1,33 @@
+import { TemplateFn, CHANGE_TYPE, css } from 'wc-lib';
+import { SanderRonde } from './sander-ronde.js';
+import { html, render } from 'lit-html';
+
+export const SanderRondeCSS = new TemplateFn<SanderRonde>(
+	function () {
+		return html`
+			<style>
+				${css(this).$.scroller} {
+					width: 100vw;
+					height: 100vh;
+					overflow-x: hidden;
+					overflow-y: auto;
+				}
+
+				${css(this).$.scroller.pseudo(':-webkit-scrollbar')} {
+					width: 0.5em;
+					height: 0.5em;
+				}
+
+				${css(this).$.scroller.pseudo(':-webkit-scrollbar-thumb')} {
+					background: #373c49;
+				}
+
+				${css(this).$.scroller.pseudo(':-webkit-scrollbar-track')} {
+					background: none;
+				}
+			</style>
+		`;
+	},
+	CHANGE_TYPE.NEVER,
+	render
+);
