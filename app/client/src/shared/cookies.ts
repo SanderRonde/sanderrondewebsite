e9c048108cbafa03ddb902a2b11d4ff70fd515ec
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-22 03:17:36 +0200

    add dedicated cookie file

---
+++ app/client/src/shared/cookies.ts
@@ -0,0 +1,40 @@
+function getCookieValue(name: string): string | undefined {
+	if (typeof document === undefined || !('cookie' in document)) {
+		return undefined;
+	}
+
+	const cookies = document.cookie.split(';').map((cookieString) => {
+		const [key, ...rest] = cookieString.trim().split('=');
+		return {
+			key,
+			value: rest.join('='),
+		};
+	});
+
+	for (const { key, value } of cookies) {
+		if (key === name) return value;
+	}
+
+	return undefined;
+}
+
+export function getCookie(name: string): string | undefined;
+export function getCookie<V extends string>(
+	name: string,
+	defaultValue: V
+): string | V;
+export function getCookie<V extends string>(
+	name: string,
+	defaultValue?: V
+): string | V | undefined {
+	const cookieValue = getCookieValue(name);
+	if (!cookieValue && defaultValue) {
+		setCookie(name, defaultValue);
+		return defaultValue;
+	}
+	return cookieValue;
+}
+
+export function setCookie(name: string, value: string) {
+	document.cookie = `${document.cookie}; ${name}=${value}`;
+}
