b40625b39268ea5efebe640160e46022ec541dcb
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-04-30 00:29:51 +0200

    move card

--- app/client/src/components/shared/elevated-card/elevated-card.ts
+++ app/client/src/components/shared/elevated-card/elevated-card.ts
@@ -2,6 +2,9 @@ import { ConfigurableWebComponent, Props, config } from 'wc-lib';
 import { IDMapType, ClassMapType } from './elevated-card-querymap';
 import { ElevatedCardHTML } from './elevated-card.html.js';
 import { ElevatedCardCSS } from './elevated-card.css.js';
+import { I18NType } from '../../../../../i18n/i18n-defs';
+import { themes } from '../../../../../shared/theme';
+import { LANGUAGE } from '../../../../../i18n/i18n';
 
 @config({
 	is: 'elevated-card',
@@ -12,7 +15,10 @@ export class ElevatedCard extends ConfigurableWebComponent<{
 	selectors: {
 		IDS: IDMapType;
 		CLASSES: ClassMapType;
-	}
+	};
+	langs: LANGUAGE;
+	i18n: I18NType;
+	themes: typeof themes;
 }> {
 	props = Props.define(this, {
 		// ...
