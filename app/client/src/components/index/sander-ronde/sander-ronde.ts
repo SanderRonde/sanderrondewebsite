6166d4a379166f5474116fe89d75f2b706855536
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-05-09 18:03:44 +0200

    fix merge issue

--- app/client/src/components/index/sander-ronde/sander-ronde.ts
+++ app/client/src/components/index/sander-ronde/sander-ronde.ts
@@ -7,7 +7,6 @@ import { ConfigurableWebComponent, config } from 'wc-lib';
 import { I18NType } from '../../../../../i18n/i18n-defs';
 import { SanderRondeHTML } from './sander-ronde.html.js';
 import { NameBlock } from '../name-block/name-block.js';
-import { InfoBlock } from '../info-block/info-block.js';
 import { themes } from '../../../../../shared/theme.js';
 import { InfoBlock } from '../info-block/info-block.js';
 import { SanderRondeCSS } from './sander-ronde.css.js';
