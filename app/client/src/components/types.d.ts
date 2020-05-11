c919e68bea0077acc09b1c5b57d7fd1fae9d6435
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-05-11 18:52:55 +0200

    add definition

--- app/client/src/components/types.d.ts
+++ app/client/src/components/types.d.ts
@@ -3,8 +3,10 @@ import { SingleTimeline } from './index/single-timeline/single-timeline';
 import { JoinedTimeline } from './index/joined-timeline/joined-timeline';
 import { NameBlock } from './index/name-block/name-block';
 import { InfoBlock } from './index/info-block/info-block';
+import { RawHTML } from './shared/raw-html/raw-html';
 
 declare global {
+	type HTMLRawHtmlElement = RawHTML;
 	type HTMLNameBlockElement = NameBlock;
 	type HTMLInfoBlockElement = InfoBlock;
 	type HTMLSingleTimelineElement = SingleTimeline;
