a133bdb9077c63d5098bf647362aaa28f8dea91a
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-06-01 04:34:23 +0200

    add all education, work and part of the personal projects

--- app/client/src/config/me.ts
+++ app/client/src/config/me.ts
@@ -215,7 +215,10 @@ export type InternationalText =
 	  }
 	| string;
 
-function getInternationText(text: InternationalText, lang: LANGUAGE): string {
+export function getInternationText(
+	text: InternationalText,
+	lang: LANGUAGE
+): string {
 	if (typeof text === 'string') return text;
 	return text[lang];
 }
@@ -228,12 +231,17 @@ export namespace LifeTimeline {
 		EDUCATION = 'education',
 	}
 
+	export const enum END_DATE {
+		NEVER = 'never',
+		TBD = 'tbd',
+	}
+
 	interface BaseEntry {
 		start: Date;
-		end: Date;
+		end: Date | END_DATE;
 		type: TYPE;
 		skills: Skill.SKILL[];
-		icon?: string;
+		icon?: string[];
 		title: InternationalText;
 		description: InternationalText;
 	}
@@ -255,11 +263,13 @@ export namespace LifeTimeline {
 	export interface EducationEntry extends BaseEntry {
 		type: TYPE.EDUCATION;
 		school: InternationalText;
-		schoolURL?: InternationalText;
+		schoolURL?: InternationalText[];
 	}
 
 	export interface WorkEntry extends BaseEntry {
 		type: TYPE.WORK;
+		employer: string;
+		employerURL?: InternationalText;
 	}
 
 	export type Entry =
@@ -274,7 +284,7 @@ export namespace LifeTimeline {
 		{
 			type: TYPE.EDUCATION,
 			school: 'Strabrecht College, Geldrop',
-			schoolURL: 'https://www.strabrecht.nl/',
+			schoolURL: ['https://www.strabrecht.nl/'],
 			start: new Date(2008, 8),
 			end: new Date(2014, 5),
 			skills: [],
@@ -290,14 +300,15 @@ export namespace LifeTimeline {
 				en: 'Leiden University, Leiden',
 				nl: 'Universiteit Leiden, Leiden',
 			},
-			schoolURL: {
-				en: 'https://www.universiteitleiden.nl/en',
-				nl: 'https://www.universiteitleiden.nl/',
-			},
+			schoolURL: [
+				{
+					en: 'https://www.universiteitleiden.nl/en',
+					nl: 'https://www.universiteitleiden.nl/',
+				},
+			],
 			start: new Date(2014, 8),
 			end: new Date(2018, 2),
-			// TODO: icon
-			icon: '',
+			icon: ['/timeline/leiden_university.png'],
 			title: {
 				en: 'Bachelor in Computer Science',
 				nl: 'Bachelor Informatica',
@@ -331,7 +342,7 @@ export namespace LifeTimeline {
 				en:
 					'Bachelor thesis exploring the application of recurrent neural networks in detecting anomalies in user behavior on computer networks',
 				nl:
-					'Bachelor thesis waarin onderzocht wordt hoe recurrente neurale netwerken gebruikt kunnen worden voor het detecteren van abnormaal gedrag in computer netwerken',
+					'Bachelor thesis waarin onderzocht wordt hoe recurrente neurale netwerken gebruikt kunnen worden voor het detecteren van abnormaal gedrag in computernetwerken',
 			},
 			skills: [
 				Skill.SKILL.PYTHON,
@@ -339,6 +350,174 @@ export namespace LifeTimeline {
 				Skill.SKILL.LATEX,
 				Skill.SKILL.KERAS,
 			],
+		},
+		{
+			type: TYPE.EDUCATION,
+			school: {
+				en: 'University of Amsterdam & Vrije Universiteit Amsterdam',
+				nl: 'Universiteit van Amsterdam & Vrije Universiteit Amsterdam',
+			},
+			schoolURL: [
+				{
+					en: 'https://www.uva.nl/en',
+					nl: 'https://www.uva.nl/',
+				},
+				{
+					en: 'https://www.vu.nl/en/',
+					nl: 'https://www.vu.nl/',
+				},
+			],
+			start: new Date(2018, 3),
+			end: END_DATE.TBD,
+			icon: ['/timeline/uva.png', '/timeline/vu.png'],
+			title: {
+				en: 'Master in Computer Science',
+				nl: 'Master in Computer Science',
+			},
+			description: {
+				en:
+					'Computer science master, joint degree between both the UvA & VU. Following the Computer Systems Security track',
+				nl:
+					'Computer science master, joint degree tussen de UvA en VU. Gevolgde track is de Computer Systems Security track',
+			},
+			skills: [
+				Skill.SKILL.C,
+				Skill.SKILL.ASSEMBLY,
+				Skill.SKILL.BASH,
+				Skill.SKILL.IDA_PRO,
+				Skill.SKILL.LINUX,
+				Skill.SKILL.CSS,
+				Skill.SKILL.HTML,
+				Skill.SKILL.JAVASCRIPT,
+				Skill.SKILL.TYPESCRIPT,
+				Skill.SKILL.REACT,
+				Skill.SKILL.PYTHON,
+			],
+		},
+		{
+			type: TYPE.WORK,
+			start: new Date(2017, 11),
+			end: new Date(2019, 7),
+			skills: [],
+			title: {
+				en: 'Computer help',
+				nl: 'Computer hulp',
+			},
+			employer: 'Studentaanhuis',
+			employerURL: 'https://www.studentaanhuis.nl/',
+			description: {
+				en: 'Fixing various computer- and smartphone related problems',
+				nl:
+					'Oplossen van verschillende computer- en smartphone gerelateerde problemen',
+			},
+			icon: ['/timeline/sah.png'],
+		},
+		{
+			type: TYPE.WORK,
+			start: new Date(2019, 7),
+			end: END_DATE.TBD,
+			skills: [
+				Skill.SKILL.CSS,
+				Skill.SKILL.DJANGO,
+				Skill.SKILL.HTML,
+				Skill.SKILL.JAVASCRIPT,
+				Skill.SKILL.TYPESCRIPT,
+				Skill.SKILL.PYTHON,
+				Skill.SKILL.REACT,
+			],
+			employer: 'Nextup Software',
+			employerURL: 'https://nextupsoftware.com/',
+			title: 'Full stack developer',
+			description: {
+				en:
+					'Maintaining and expanding on a transport management system witha  frontend written in React, MobX and typescript and a backend using Django Python. Additionally managing the security of the full system. Mainly consists of ensuring test coverage, regular pentests and general security advice.',
+				nl:
+					'Onderhouden en uitbreiden van een transportmanagement systeem met een frontend in React, MobX en Typescript en een backend in Django Python. Daarnaast het beheren van de security van het volledige systeem. Bestaat voornamelijk uit het zorgen voor test coverage, regelmatige pentests en algemeen security advies.',
+			},
+			icon: ['/timeline/nextup.png'],
+		},
+		{
+			type: TYPE.PERSONAL_PROJECT,
+			start: new Date(2015, 3),
+			end: END_DATE.NEVER,
+			source: 'https://github.com/SanderRonde/BinderApp',
+			url:
+				'https://chrome.google.com/webstore/detail/binder-app/jeolbigkboigkhlfhilmedbakkkcmeif',
+			skills: [
+				Skill.SKILL.BROWSER_EXTENSIONS,
+				Skill.SKILL.CSS,
+				Skill.SKILL.HTML,
+				Skill.SKILL.JAVASCRIPT,
+			],
+			title: 'Binder App (browser app)',
+			description: {
+				en:
+					'A browser app that allows you to quickly open websites from your desktop by binding keywords to websites. Also implements some omnibar features like searching search engines.',
+				nl:
+					'Een browser app waarmee je snel en makkelijk websites kan openen vanaf je desktop door bepaalde keywords aan websites te binden. Bevat ook een aantal omnibar features zoals het doorzoeken van zoekmachines.',
+			},
+			icon: ['/timeline/binder_app.png'],
+		},
+		{
+			type: TYPE.PERSONAL_PROJECT,
+			start: new Date(2016, 10),
+			end: END_DATE.NEVER,
+			source: 'https://github.com/SanderRonde/WatchMeType',
+			skills: [
+				Skill.SKILL.CSS,
+				Skill.SKILL.HTML,
+				Skill.SKILL.TYPESCRIPT,
+				Skill.SKILL.NODE,
+				Skill.SKILL.REACT,
+			],
+			title: 'WatchMeType',
+			description: {
+				en:
+					'A web app that allows you to type using nothing but your finger and a leap motion. Created as a demo for what the leap motion could do for typing on a smartwatch.',
+				nl:
+					'Een browser app waarmee je kan typen met enkel je vinger en een leap motion. Gemaakt als een demo voor wat de leap motion zou kunnen doen voor het typen op een smartwatch',
+			},
+		},
+		{
+			type: TYPE.PERSONAL_PROJECT,
+			start: new Date(2016, 10),
+			end: END_DATE.NEVER,
+			source: 'https://github.com/SanderRonde/WatchMeType',
+			skills: [
+				Skill.SKILL.CSS,
+				Skill.SKILL.HTML,
+				Skill.SKILL.TYPESCRIPT,
+				Skill.SKILL.NODE,
+				Skill.SKILL.REACT,
+			],
+			title: 'WatchMeType',
+			description: {
+				en:
+					'A web app that allows you to type using nothing but your finger and a leap motion. Created as a demo for what the leap motion could do for typing on a smartwatch.',
+				nl:
+					'Een browser app waarmee je kan typen met enkel je vinger en een leap motion. Gemaakt als een demo voor wat de leap motion zou kunnen doen voor het typen op een smartwatch',
+			},
+		},
+		{
+			type: TYPE.PERSONAL_PROJECT,
+			// TODO:
+			start: new Date(2016, 10),
+			end: END_DATE.NEVER,
+			source: 'https://github.com/SanderRonde/sanderrondewebsite',
+			url: 'http://sanderron.de/',
+			skills: [
+				Skill.SKILL.CSS,
+				Skill.SKILL.HTML,
+				Skill.SKILL.TYPESCRIPT,
+				Skill.SKILL.NODE,
+			],
+			title: 'sanderron.de',
+			// TODO: determine min size needed
+			icon: ['/icons/128.png'],
+			description: {
+				en: "The website you're reading this on",
+				nl: 'De website waarop je dit leest',
+			},
 		},
 	];
 }
