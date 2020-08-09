3b1f92bafeff7fc1d57d05eba120f6eedf9f0260
Sander Ronde <awsdfgvhbjn@gmail.com>
2020-08-09 06:20:03 +0200

    cache resolved manifest.json

--- app/server/lib/semi-static-files.ts
+++ app/server/lib/semi-static-files.ts
@@ -1,13 +1,27 @@
 import { manifestJSON } from '../semi-static/manifest.json.js';
 import { RequestVars } from './request-vars.js';
 import { CACHE_HEADER } from './constants.js';
+import { THEME } from '../../shared/theme.js';
 import { WebServer } from '../app.js';
+import { Caching } from './cache.js';
 
 export namespace SemiStaticFiles {
 	export function initRoutes({ app }: WebServer) {
+		const manifestCache = Caching.createStore<
+			THEME,
+			{
+				content: string;
+				mime: string;
+			}
+		>(false);
 		app.get('/manifest.json', (req, res) => {
 			const theme = RequestVars.getTheme(req, res);
-			const { content, mime } = manifestJSON(theme);
+			const { content, mime } = (() => {
+				if (manifestCache.has(theme)) {
+					return manifestCache.get(theme)!;
+				}
+				return manifestJSON(theme);
+			})();
 			res.contentType(mime);
 			res.set('Cache-Control', CACHE_HEADER);
 			res.end(content);
